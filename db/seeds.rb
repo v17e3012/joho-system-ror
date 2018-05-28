46c27533244c28247af993e8e52137ee450a719d
Tojo <v17e3012@oita-u.ac.jp>
2018-05-28 02:28:16 +0000

    lesson 05-07, #3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do |i|
+    Post.create(title: "title #{i}", body: "body #{i}")
+end
