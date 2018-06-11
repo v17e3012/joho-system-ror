8e04270bb609c9d5a468d8c6e92fc293ac3bb467
Tojo <v17e3012@oita-u.ac.jp>
2018-06-11 04:57:01 +0000

    lesson 07

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,17 @@ class PostsController < ApplicationController
     def index  
         @posts = Post.all.order(created_at: 'desc')
     end
+    
+    def show
+       @post = Post.find(params[:id])
+    end
+    
+    def new
+        
+    end
+    
+    def create
+       
+    end
+    
 end
