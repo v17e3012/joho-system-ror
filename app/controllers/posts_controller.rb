31fd1d17509b08aaf6a66f06ca969f590bb92656
Tojo <v17e3012@oita-u.ac.jp>
2018-06-18 04:17:13 +0000

    lesson 08

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,23 @@ class PostsController < ApplicationController
     end
     
     def new
-        
+        @post = Post.new
     end
     
     def create
-       
+       # save
+       @post = Post.new(post_params)
+       if @post.save
+           # redirect
+           redirect_to posts_path
+       else
+           render 'new'
+       end
     end
+    
+    private
+        def post_params
+            params.require(:post).permit(:title, :body)
+        end
     
 end
